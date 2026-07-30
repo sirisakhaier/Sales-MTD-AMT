f48c3614a56ed2508c814594554b31663e209416
sirisakhaier <sirisakhaier@users.noreply.github.com>
2026-07-30 16:18:13 +0700

    Fix next.config.js for Next.js 15 - move serverExternalPackages

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    serverComponentsExternalPackages: ['better-sqlite3']
-  }
+  serverExternalPackages: ['better-sqlite3']
 };
 
 module.exports = nextConfig;
